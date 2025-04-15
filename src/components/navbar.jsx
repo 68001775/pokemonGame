e837abcec8c3b7bb9efd9bc68e80b49527e0b150
Lukah Youngs <lukahyoungs@gmail.com>
2025-04-15 10:06:59 -0400

    Navbar Update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,33 +1,55 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
 import "../css/navbar.css";
 
 function NavBar({ doomDoubloons }) {
+  const location = useLocation();
+  const [activeLeft, setActiveLeft] = useState(0);
+  const itemRefs = useRef([]);
+
+  const paths = ["/", "/Collection", "/base1", "/sv045", "/swsh12_5", "/sv04"];
+
+  useEffect(() => {
+    const index = paths.indexOf(location.pathname);
+    if (index !== -1 && itemRefs.current[index]) {
+      const item = itemRefs.current[index];
+      const offsetLeft = item.offsetLeft;
+      setActiveLeft(offsetLeft);
+    }
+  }, [location.pathname]);
+
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">
-          Home {doomDoubloons !== undefined ? `(${doomDoubloons})` : ""}
-        </Link>
-      </div>
-      <div className="navbar-links">
-        <Link to="/Collection" className="nav-ink">
-          Collection
-        </Link>
-        <Link to="/base1" className="nav-ink">
-          Base Set
-        </Link>
-        <Link to="/sv045" className="nav-ink">
-          Paldean Fates
-        </Link>
-        <Link to="/swsh12_5" className="nav-ink">
-          Crown Zenith
-        </Link>
-        <Link to="/sv04" className="nav-ink">
-          Paradox Rift
-        </Link>
-      </div>
-    </nav>
+    <div className="container">
+      <nav className="navbar">
+        <ul className="nav--list">
+          <div className="active" style={{ left: `${activeLeft}px` }}></div>
+
+          {paths.map((path, index) => (
+            <li
+              key={path}
+              className="item"
+              ref={(el) => (itemRefs.current[index] = el)}
+            >
+              <Link to={path}>
+                {path === "/"
+                  ? `Home ${
+                      doomDoubloons !== undefined ? `(${doomDoubloons})` : ""
+                    }`
+                  : path === "/Collection"
+                  ? "Collection"
+                  : path === "/base1"
+                  ? "Base Set"
+                  : path === "/sv045"
+                  ? "Paldean Fates"
+                  : path === "/swsh12_5"
+                  ? "Crown Zenith"
+                  : "Paradox Rift"}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
